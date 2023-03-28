1463da566c0c953cef258d0fdf5e0641310cd75a
Elisa <elisamguiar@gmail.com>
2023-03-28 17:51:19 -0300

    .

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -31,40 +31,35 @@ export const register = (req, res) => {
 }
 
 export const login = (req, res) => {
-    /* const q = "SELECT * FROM gerentes WHERE grt_email = ?"
+    const q = "SELECT * FROM usu_usuarios WHERE usu_email = ?"
 
-    db.query(q, [req.body.email], (err, data) => {
+    db.query(q, [req.body.emaillogin], (err, data) => {
         if (err) {
             return res.json(err)
-        }
+        } 
         if (data.length === 0) {
-            const q = "SELECT * FROM vendedores WHERE vdd_email = ?"
-
-            db.query(q, [req.body.email], (err, data) => {
-                if (err) {
-                    return res.json(err)
-                }
-                if (data.length === 0) {
-                    return res.status(404).json("Usuário não encontrado")
-                } else {
-                    const senhaCorreta = bcrypt.compareSync(req.body.senha, data[0].vdd_senha)
-
-                    if (!senhaCorreta) {
-                        return res.status(400).json("Email ou senha incorretos!")
-                    }
-                }
-            })
-
+            return res.status(400).json("Email ou senha incorretos!")
         } else {
-            const senhaCorreta = bcrypt.compareSync(req.body.senha, data[0].grt_senha)
+            const senhaCorreta = bcrypt.compareSync(req.body.senhalogin, data[0].usu_senha)
 
             if (!senhaCorreta) {
                 return res.status(400).json("Email ou senha incorretos!")
+            } else {
+                var token = jwt.sign({ id: data[0].usu_id }, 'jwtSecurity')
+                const { usu_senha, usu_id, ...other } = data[0]
+
+                res.cookie("token", token, {
+                    httpOnly: true
+                })
+                return res.status(200).json(other)
             }
         }
     })
-    */
 }
 
 export const logout = (req, res) => {
+    res.clearCookie("token", {
+        sameSite: "none",
+        secure: true
+    }).status(200).json("Usuário deslogado.")
 }
--- server/main.js
+++ server/main.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.js'
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}))
+
+app.use(cookieParser())
 
 app.use(express.json())
 app.use('/api', authRoutes)
